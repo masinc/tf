80a58369204c52c2a8141a6f4697affd4ff54c58
masinc <masinc000@gmail.com>
2024-07-15 19:44:37 +0900

    chore: Update Terraform provider version

--- providers.tf
+++ providers.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = "~> 1"
+
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
@@ -8,5 +10,5 @@ terraform {
 }
 
 provider "cloudflare" {
-  
+
 }
