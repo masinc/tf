ffce4edba52ee1174a15405adce162a449781bc4
masinc <masinc000@gmail.com>
2024-07-15 19:46:04 +0900

    chore: Delete variables.tf for Cloudflare Account ID

--- variables.tf
+++ variables.tf
@@ -1,13 +1,6 @@
 variable "cloudflare_account_id" {
-    description = "Cloudflare Account ID"
-    sensitive = true
-    type = string
-    nullable = false    
-}
-
-variable "cloudflare_zone_id_masinc_dev" {
-    description = "Cloudflare Zone ID for masinc.dev"
-    sensitive = true
-    type = string
-    nullable = false
+  description = "Cloudflare Account ID"
+  sensitive   = true
+  type        = string
+  nullable    = false
 }
